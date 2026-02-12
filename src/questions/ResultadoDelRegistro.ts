8786a443efd76008272f9b34a4c077b9d129ec0b
miguel-angel-dev18 <miguel.angel.dev.18@gmail.com>
2026-02-11 21:34:05 -0500

    fix: sincronizar captura de texto en Screenplay y habilitar workflow_dispatch

--- src/questions/ResultadoDelRegistro.ts
+++ src/questions/ResultadoDelRegistro.ts
@@ -1,10 +1,18 @@
 import { Page } from '@playwright/test';
 import { RegistroUI } from '../ui/RegistroUI';
+import { expect } from '@playwright/test';
 
 export class ResultadoDelRegistro {
-  //obtener el mensaje
-  static async textoVisible(page: Page): Promise<string | null> {
+  static async textoVisible(page: Page): Promise<string> {
     const locator = page.locator(RegistroUI.mensajeResultado);
-    return await locator.textContent();
+    
+    // 1. Espera a que el elemento aparezca (Estado visible)
+    await locator.waitFor({ state: 'visible' });
+
+    // 2. REINTENTO AUTOMÁTICO
+    await expect(locator).not.toHaveText("", { timeout: 10000 }); 
+
+    const texto = await locator.textContent();
+    return texto?.trim() || "";
   }
 }
